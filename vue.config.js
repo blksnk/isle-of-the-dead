b9658203a06bc1ebd0b0296546d82bee54490638
JN Veigel <art.jnveigel@gmail.com>
2020-11-21 00:28:59 +0100

    :bug: remove baseUrl

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
   ? '/isle-of-the-dead/'
-  : '/',
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? '/isle-of-the-dead/'
-    : '/'
+  : '/'
 }
