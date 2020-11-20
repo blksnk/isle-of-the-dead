9feda8e54a1ba8c2ac532e141f867c972c41e264
JN Veigel <art.jnveigel@gmail.com>
2020-11-21 00:07:19 +0100

    deploy

---
+++ precache-manifest.0d43f895f903c20fc71578317a82f034.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "643bd911279f9c388313",
+    "url": "/isle-of-the-dead/css/app.36b89702.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/isle-of-the-dead/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ebf278fab7849bb988a95778a07cb398",
+    "url": "/isle-of-the-dead/index.html"
+  },
+  {
+    "revision": "242a282bbf184721ab0b",
+    "url": "/isle-of-the-dead/js/about.85b918ff.js"
+  },
+  {
+    "revision": "643bd911279f9c388313",
+    "url": "/isle-of-the-dead/js/app.90b42729.js"
+  },
+  {
+    "revision": "de47a3272695399734bb",
+    "url": "/isle-of-the-dead/js/chunk-vendors.705fa262.js"
+  },
+  {
+    "revision": "1ad9b594ad5c040491cb55379b0479bf",
+    "url": "/isle-of-the-dead/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/isle-of-the-dead/robots.txt"
+  }
+]);
