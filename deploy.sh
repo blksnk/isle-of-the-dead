677c8c42006d9391d8fe643d548d46e33dd3a146
JN Veigel <art.jnveigel@gmail.com>
2020-11-21 00:15:07 +0100

    marche bordel

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:username/isle-of-the-dead.git master:gh-pages
+git push -f git@github.com:blksnk/isle-of-the-dead.git master:gh-pages
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/isle-of-the-dead/'
+  : '/',
+  baseUrl: process.env.NODE_ENV === 'production'
     ? '/isle-of-the-dead/'
     : '/'
 }
