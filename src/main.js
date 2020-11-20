06d5c5ed83cd171af11cddbda52f82fa2b34d6df
JN Veigel <art.jnveigel@gmail.com>
2020-11-21 00:45:32 +0100

    remove router

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import dotenv from 'dotenv';
 import App from "./App.vue";
 import "./registerServiceWorker";
-import router from "./router";
 import store from "./store";
 import slide from './components/Slide.vue'
 
@@ -14,7 +13,6 @@ Vue.component('slide', slide);
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
   store,
   render: h => h(App)
 }).$mount("#app");
