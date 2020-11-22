1c48cd94872edc720a1606f4056e8011c9af2121
JN Veigel <art.jnveigel@gmail.com>
2020-11-22 18:25:33 +0100

    change password login default behavior

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div id='title'>
+    <div id="title">
       <h2>Isle of the Dead, Arnold Böcklin</h2>
     </div>
     <div id="sectionTitle">
@@ -17,9 +17,17 @@
       <button @click="prevSlide">Previous</button>
       <button @click="nextSlide">Next</button>
     </div>
-    <div id='adminLogin' v-if="!isAdmin && showAdminLogin">
+    <div id="slideCounter" v-if="!isAdmin && !showAdminLogin">
+      <span>Slide {{ slideIndex + 1 }} of {{ slides.length }}</span>
+      <button @click="showAdminLogin = true">admin login</button>
+    </div>
+    <div id="adminLogin" v-if="!isAdmin && showAdminLogin">
       <form @submit.prevent="submitLogin">
-        <input type="password" placeholder="admin password">
+        <input
+          type="password"
+          autocomplete="off"
+          placeholder="admin password"
+        />
       </form>
       <button @click="showAdminLogin = false">hide</button>
     </div>
@@ -27,231 +35,238 @@
 </template>
 
 <script>
-//packages
-import { io } from 'socket.io-client'
+  //packages
+  import { io } from 'socket.io-client';
 
-//components
-import Painting from '@/components/Painting'
+  //components
+  import Painting from '@/components/Painting';
 
-//slides
-import Intro from '@/components/slides/Intro'
-import DescSubjects from '@/components/slides/DescSubjects'
-import DescTechnique from '@/components/slides/DescTechnique'
+  //slides
+  import Intro from '@/components/slides/Intro';
+  import DescSubjects from '@/components/slides/DescSubjects';
+  import DescTechnique from '@/components/slides/DescTechnique';
 
-export default {
-  name: 'App',
-  components: {
-    Intro,
-    Painting,
-  },
-  data() {
-    return {
-      slideIndex: 0,
-      reveal: false,
-      socket: null,
-      showAdminLogin: true,
-      isAdmin: false,
-      slides: [{
-          title: 'Introduction',
-          component: Intro,
-        },
-        {
-          title: 'Description \u2014 Subjects',
-          component: DescSubjects
-        },
-        {
-          title: 'Description \u2014 Technique',
-          component: DescTechnique
-        },
-      ],
-      images: [{
-          url: 'https://i0.wp.com/www.sensesatlas.com/wp-content/uploads/2020/04/Arnold_B%C3%B6cklin_-_Die_Toteninsel_I_Kunstmuseum_Basel.jpg?ssl=1',
-          date: 1880,
-        },
-        {
-          url: 'https://i0.wp.com/www.sensesatlas.com/wp-content/uploads/2020/04/Arnold_B%C3%B6cklin_-_Die_Toteninsel_II_Metropolitan_Museum_of_Art-scaled.jpg?resize=2048%2C1243&ssl=1',
-          date: 1880,
-        },
-        {
-          url: 'https://i2.wp.com/www.sensesatlas.com/wp-content/uploads/2020/04/Arnold_B%C3%B6cklin_-_Die_Toteninsel_III_Alte_Nationalgalerie_Berlin-scaled.jpg?resize=2048%2C1091&ssl=1',
-          date: 1883,
-        },
-        {
-          url: 'https://i1.wp.com/www.sensesatlas.com/wp-content/uploads/2020/04/Arnold-B%C3%B6cklin-The-Isle-of-the-Dead-Die-Toteninsel-The-fourth-version.jpg?w=1417&ssl=1',
-          date: 1884,
-        },
-        {
-          url: 'https://i1.wp.com/www.sensesatlas.com/wp-content/uploads/2020/04/1920px-Arnold_B%C3%B6cklin_-_Die_Toteninsel_V_Museum_der_bildenden_K%C3%BCnste_Leipzig.jpg?w=1920&ssl=1',
-          date: 1886,
-        },
-      ],
-    }
-  },
-  computed: {
-    currentSlide() {
-      return this.slides[this.slideIndex]
+  export default {
+    name: 'App',
+    components: {
+      Intro,
+      Painting,
     },
-    slideCount() {
-      return this.slides.length
-    }
-  },
-  methods: {
-    submitLogin(e) {
-      const {value} = e.target.children[0]
-      if(value === process.env.VUE_APP_ADMIN_PASSWORD) {
-        this.isAdmin = true;
-      }
+    data() {
+      return {
+        slideIndex: 0,
+        reveal: false,
+        socket: null,
+        showAdminLogin: false,
+        isAdmin: false,
+        slides: [
+          {
+            title: 'Introduction',
+            component: Intro,
+          },
+          {
+            title: 'Description \u2014 Subjects',
+            component: DescSubjects,
+          },
+          {
+            title: 'Description \u2014 Technique',
+            component: DescTechnique,
+          },
+        ],
+        images: [
+          {
+            url:
+              'https://i0.wp.com/www.sensesatlas.com/wp-content/uploads/2020/04/Arnold_B%C3%B6cklin_-_Die_Toteninsel_I_Kunstmuseum_Basel.jpg?ssl=1',
+            date: 1880,
+          },
+          {
+            url:
+              'https://i0.wp.com/www.sensesatlas.com/wp-content/uploads/2020/04/Arnold_B%C3%B6cklin_-_Die_Toteninsel_II_Metropolitan_Museum_of_Art-scaled.jpg?resize=2048%2C1243&ssl=1',
+            date: 1880,
+          },
+          {
+            url:
+              'https://i2.wp.com/www.sensesatlas.com/wp-content/uploads/2020/04/Arnold_B%C3%B6cklin_-_Die_Toteninsel_III_Alte_Nationalgalerie_Berlin-scaled.jpg?resize=2048%2C1091&ssl=1',
+            date: 1883,
+          },
+          {
+            url:
+              'https://i1.wp.com/www.sensesatlas.com/wp-content/uploads/2020/04/Arnold-B%C3%B6cklin-The-Isle-of-the-Dead-Die-Toteninsel-The-fourth-version.jpg?w=1417&ssl=1',
+            date: 1884,
+          },
+          {
+            url:
+              'https://i1.wp.com/www.sensesatlas.com/wp-content/uploads/2020/04/1920px-Arnold_B%C3%B6cklin_-_Die_Toteninsel_V_Museum_der_bildenden_K%C3%BCnste_Leipzig.jpg?w=1920&ssl=1',
+            date: 1886,
+          },
+        ],
+      };
     },
-    initSocket() {
-      const serverUrl = process.env.VUE_APP_SERVER_URL;
-      this.socket = io(serverUrl);
+    computed: {
+      currentSlide() {
+        return this.slides[this.slideIndex];
+      },
+      slideCount() {
+        return this.slides.length;
+      },
+    },
+    methods: {
+      submitLogin(e) {
+        const { value } = e.target.children[0];
+        if (value === process.env.VUE_APP_ADMIN_PASSWORD) {
+          this.isAdmin = true;
+        }
+      },
+      initSocket() {
+        const serverUrl = process.env.VUE_APP_SERVER_URL;
+        this.socket = io(serverUrl);
 
-      this.socket.on('slideChange', ({ index }) => {
-        this.setSlideIndex(index)
-      })
+        this.socket.on('slideChange', ({ index }) => {
+          this.setSlideIndex(index);
+        });
+      },
+      setSlideIndex(index) {
+        this.slideIndex = index;
+        this.revealVersions(index);
+      },
+      revealVersions(index) {
+        if (index > 1 && !this.reveal) {
+          this.reveal = true;
+        }
+      },
+      emitSlideChange(index) {
+        this.revealVersions(index);
+        if (this.isAdmin) {
+          this.socket.emit('setSlideIndex', { index });
+        }
+      },
+      nextSlide() {
+        if (this.slideIndex < this.slideCount - 1) {
+          this.slideIndex += 1;
+        }
+        this.emitSlideChange(this.slideIndex);
+      },
+      prevSlide() {
+        if (this.slideIndex > 0) {
+          this.slideIndex -= 1;
+        }
+        this.emitSlideChange(this.slideIndex);
+      },
     },
-    setSlideIndex(index) {
-      this.slideIndex = index;
-      this.revealVersions(index);
+    created() {
+      this.initSocket();
     },
-    revealVersions(index) {
-      if (index > 1 && !this.reveal) {
-        this.reveal = true;
-      }
-    },
-    emitSlideChange(index) {
-      this.revealVersions(index);
-      if(this.isAdmin) {
-        this.socket.emit('setSlideIndex', {index})
-      }
-    },
-    nextSlide() {
-      if (this.slideIndex < this.slideCount - 1) {
-        this.slideIndex += 1
-      }
-      this.emitSlideChange(this.slideIndex)
-    },
-    prevSlide() {
-      if (this.slideIndex > 0) {
-        this.slideIndex -= 1
-      }
-      this.emitSlideChange(this.slideIndex)
-    },
-  },
-  created() {
-    this.initSocket()
-  }
-};
+  };
 </script>
 
 <style lang="scss">
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-  color: #212122;
-  border-color: #212122;
-  font-family: 'Space Grotesk', sans-serif;
-}
-
-body,
-html {
-  overflow: hidden;
-}
-
-h3 {
-  text-decoration: underline;
-  margin-bottom: 2rem;
-}
-
-p {
-  margin-bottom: 1rem;
-}
-
-button,
-p,
-span {
-  font-size: 1rem;
-  line-height: 1.3rem;
-}
-
-ol {
-  padding-left: 2rem;
-}
-
-@mixin between {
-  display: flex;
-  justify-content: space-between;
-  align-items: baseline;
-}
-
-@mixin border($pos: bottom) {
-  border-#{$pos}: 1px solid #212122;
-  padding-#{$pos}: .5rem;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  background-color: #CECECE;
-
-  display: grid;
-  grid-template-rows: auto 1fr auto;
-  grid-template-columns: 6fr 4fr;
-  grid-gap: 1rem;
-  height: 100vh;
-  max-height: 100vh;
-  width: 100vw;
-  padding: 1rem;
-  overflow: hidden;
-
-  #title {
-    grid-row: 1 / 1;
-    grid-column: 1 / 1;
-    border-bottom: 1px solid #212122;
-    padding-bottom: .5rem;
-    align-self: end;
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    color: #212122;
+    border-color: #212122;
+    font-family: 'Space Grotesk', sans-serif;
   }
 
-  #sectionTitle {
-    grid-row: 1 / 1;
-    grid-column: 2 / 2;
-    border-bottom: 1px solid #212122;
-    padding-bottom: .5rem;
-    align-self: end;
-
+  body,
+  html {
+    overflow: hidden;
   }
 
-  #slides {
-    grid-column: 2 / 2;
-    grid-row: 2 / 2;
+  h3 {
+    text-decoration: underline;
+    margin-bottom: 2rem;
   }
 
-  #credits {
-    grid-row: 3 / 3;
-    grid-column: 1 / 1;
-    @include border(top);
-    align-self: end;
+  p {
+    margin-bottom: 1rem;
   }
 
-  #buttons, #adminLogin {
-    @include between;
-    @include border(top);
-    grid-row: 3 / 3;
-    grid-column: 2 / 2;
+  button,
+  p,
+  span {
+    font-size: 1rem;
+    line-height: 1.3rem;
+  }
 
-    button {
-      border: 0;
-      text-decoration: underline;
-      background: none;
+  ol {
+    padding-left: 2rem;
+  }
+
+  @mixin between {
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+  }
+
+  @mixin border($pos: bottom) {
+    border-#{$pos}: 1px solid #212122;
+    padding-#{$pos}: 0.5rem;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #cecece;
+
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+    grid-template-columns: 6fr 4fr;
+    grid-gap: 1rem;
+    height: 100vh;
+    max-height: 100vh;
+    width: 100vw;
+    padding: 1rem;
+    overflow: hidden;
+
+    #title {
+      grid-row: 1 / 1;
+      grid-column: 1 / 1;
+      border-bottom: 1px solid #212122;
+      padding-bottom: 0.5rem;
+      align-self: end;
     }
 
-    input {
-      background: #EEEEEE;
-      border: 1px solid #212122;
+    #sectionTitle {
+      grid-row: 1 / 1;
+      grid-column: 2 / 2;
+      border-bottom: 1px solid #212122;
+      padding-bottom: 0.5rem;
+      align-self: end;
+    }
+
+    #slides {
+      grid-column: 2 / 2;
+      grid-row: 2 / 2;
+    }
+
+    #credits {
+      grid-row: 3 / 3;
+      grid-column: 1 / 1;
+      @include border(top);
+      align-self: end;
+    }
+
+    #buttons,
+    #adminLogin,
+    #slideCounter {
+      @include between;
+      @include border(top);
+      grid-row: 3 / 3;
+      grid-column: 2 / 2;
+
+      button {
+        border: 0;
+        text-decoration: underline;
+        background: none;
+      }
+
+      input {
+        background: #eeeeee;
+        border: 1px solid #212122;
+      }
     }
   }
-
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
-import Vue from "vue";
+import Vue from 'vue';
 import dotenv from 'dotenv';
-import App from "./App.vue";
-import "./registerServiceWorker";
-import store from "./store";
-import slide from './components/Slide.vue'
+import App from './App.vue';
+import './registerServiceWorker';
+import store from './store';
+import slide from './components/Slide.vue';
 
 dotenv.config();
 
 Vue.component('slide', slide);
 
-
 Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount('#app');
